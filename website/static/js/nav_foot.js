34399357d22aea0a89ea9a4effb379151d1a1777
Lucien Zhang <lucien_zhang@hotmail.com>
2019-12-15 23:13:47 +0900

    region

--- website/static/js/nav_foot.js
+++ website/static/js/nav_foot.js
@@ -1,4 +1,4 @@
-$('.dropdown-menu a.dropdown-toggle').on('click', function (e) {
+$('.nav_foot .dropdown-menu a.dropdown-toggle').on('click', function (e) {
     if (!$(this).next().hasClass('show')) {
         $(this).parents('.dropdown-menu').first().find('.show').removeClass('show');
     }
