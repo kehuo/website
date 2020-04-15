15119e6cd88322c7ddcd38a0aeb37551f5c88843
Lucien Zhang <lucien_zhang@hotmail.com>
2020-04-15 14:00:07 +0900

    demo mnist

--- docs/.vuepress/theme/enhanceApp.js
+++ docs/.vuepress/theme/enhanceApp.js
@@ -4,8 +4,17 @@
 // import DatePicker from 'ant-design-vue/lib/date-picker'; // 加载 JS
 // import 'ant-design-vue/lib/date-picker/style/css'; // 加载 CSS
 // Vue.use(Button);
+// 'ant-design-vue/dist/antd.css'
+// ant-design-vue/dist/antd.less
+
 import Button from 'ant-design-vue/lib/button';
 import 'ant-design-vue/lib/button/style/css';
+import Upload from 'ant-design-vue/lib/upload';
+import 'ant-design-vue/lib/upload/style/css';
+import Icon from 'ant-design-vue/lib/icon';
+import 'ant-design-vue/lib/icon/style/css';
+import Empty from 'ant-design-vue/lib/empty';
+import 'ant-design-vue/lib/empty/style/css';
 
 export default ({
     Vue, // the version of Vue being used in the VuePress app
@@ -16,4 +25,7 @@ export default ({
 }) => {
     // ...apply enhancements to the app
     Vue.use(Button);
+    Vue.use(Upload);
+    Vue.use(Icon);
+    Vue.use(Empty);
 };
