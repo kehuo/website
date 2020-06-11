5ee9755332f9e64881c031f369ba15e62535d818
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-11 17:02:31 +0900

    styles

--- docs/.vuepress/theme/enhanceApp.js
+++ docs/.vuepress/theme/enhanceApp.js
@@ -1,21 +1,3 @@
-// import Antd from 'ant-design-vue';
-// import { Button } from 'ant-design-vue';
-// import 'ant-design-vue/dist/antd.css';
-// import DatePicker from 'ant-design-vue/lib/date-picker'; // 加载 JS
-// import 'ant-design-vue/lib/date-picker/style/css'; // 加载 CSS
-// Vue.use(Button);
-// 'ant-design-vue/dist/antd.css'
-// ant-design-vue/dist/antd.less
-
-// import Button from "ant-design-vue/lib/button";
-// import "ant-design-vue/lib/button/style/css";
-// import Upload from "ant-design-vue/lib/upload";
-// import "ant-design-vue/lib/upload/style/css";
-// import Icon from "ant-design-vue/lib/icon";
-// import "ant-design-vue/lib/icon/style/css";
-// import Empty from "ant-design-vue/lib/empty";
-// import "ant-design-vue/lib/empty/style/css";
-
 import { message } from "ant-design-vue";
 
 export default ({
