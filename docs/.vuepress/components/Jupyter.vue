975e6d95f9eaf5ebf21acc0d2db3012cfc4dfa33
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-16 11:33:42 +0900

    prevent duplicated refs

--- docs/.vuepress/components/Jupyter.vue
+++ docs/.vuepress/components/Jupyter.vue
@@ -8,18 +8,27 @@
       </a>
     </p>
     <tabs v-if="fileList" @changed="tabChanged">
-      <tab v-for="(file, index) in fileList" :key="index" :name="file.name"></tab>
+      <tab v-for="(file, index) in fileList" :key="index" :name="file.name">
+        <iframe
+          frameborder="no"
+          scrolling="no"
+          :src="'/nbviewer/localfile/'+filePath"
+          :ref="'iframe-'+index"
+          @load="resizeIframe(index)"
+        ></iframe>
+      </tab>
     </tabs>
     <tabs v-else-if="srcPrefix" @changed="filePath=srcPrefix + '/' + $event.tab.name + '.ipynb'">
-      <tab v-for="(name, index) in ['python','java']" :key="index" :name="name"></tab>
+      <tab v-for="(name, index) in ['python','java']" :key="index" :name="name">
+        <iframe
+          frameborder="no"
+          scrolling="no"
+          :src="'/nbviewer/localfile/'+filePath"
+          :ref="'iframe-'+index"
+          @load="resizeIframe(index)"
+        ></iframe>
+      </tab>
     </tabs>
-    <iframe
-      frameborder="no"
-      scrolling="no"
-      :src="'/nbviewer/localfile/'+filePath"
-      ref="iframe"
-      @load="resizeIframe"
-    ></iframe>
   </div>
 </template>
 
@@ -37,19 +46,27 @@ export default {
   },
   data() {
     return {
-      filePath: ""
+      filePath: "",
+      activeIndex: null
     };
   },
   methods: {
-    resizeIframe() {
-      this.$refs.iframe.style.height =
-        this.$refs.iframe.contentWindow.document.documentElement.scrollHeight +
-        "px";
+    resizeIframe(index) {
+      if (index === undefined) {
+        index = this.activeIndex;
+      }
+      if (index === null) {
+        return;
+      }
+      let e = this.$refs["iframe-" + index];
+      e.style.height =
+        e.contentWindow.document.documentElement.scrollHeight + "px";
     },
     tabChanged(selectedTab) {
       for (const file of this.fileList) {
         if (file.name === selectedTab.tab.name) {
           this.filePath = file.path;
+          this.activeIndex = selectedTab.tab.$vnode.data.key;
           break;
         }
       }
