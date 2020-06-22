08317e78028933fdeb1e6c0f042c6e3ff826a52e
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-22 23:21:20 +0900

    add typer

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -1,17 +1,30 @@
 <template>
   <main class="home" aria-labelledby="main-title">
+    <div class="styled-font">
+      <p>I AM</p>
+      <span id="typed"></span>
+    </div>
+
     <Content class="theme-default-content custom" />
   </main>
 </template>
 
 <script>
+import Typed from "typed.js";
+
 export default {
   name: "Home",
-
   computed: {
     data() {
       return this.$page.frontmatter;
     }
+  },
+  mounted() {
+    new Typed(".home #typed", {
+      strings: ["A Coder", "A cooker", "A leaner"],
+      typeSpeed: 40,
+      loop: true
+    });
   }
 };
 </script>
@@ -22,6 +35,12 @@ export default {
   max-width: $homePageWidth;
   margin: 0px auto;
   display: block;
+
+  .styled-font {
+    font-size: 65px;
+    // color: #FFFFFF;
+    // font-family : lulo-clean-w01-one-bold, sans-serif;
+  }
 }
 
 @media (max-width: $MQMobile) {
