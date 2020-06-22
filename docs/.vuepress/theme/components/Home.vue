60a13d3f9de5e43f31008732f49d918d4f3b5ea0
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-22 11:19:49 +0900

    home page

---
+++ docs/.vuepress/theme/components/Home.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="home" aria-labelledby="main-title">
+    <Content class="theme-default-content custom" />
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  computed: {
+    data() {
+      return this.$page.frontmatter;
+    }
+  }
+};
+</script>
+
+<style lang="stylus">
+.home {
+  padding: $navbarHeight 0 0;
+  max-width: $homePageWidth;
+  margin: 0px auto;
+  display: block;
+}
+
+@media (max-width: $MQMobile) {
+  .home {
+  }
+}
+
+@media (max-width: $MQMobileNarrow) {
+  .home {
+  }
+}
+</style>
