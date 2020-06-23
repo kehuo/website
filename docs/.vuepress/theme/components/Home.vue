416e77ac210c6e360d12875d4365dd3096d42290
Lucien Zhang <lucien_zhang@hotmail.com>
2020-06-23 12:25:36 +0900

    home base

--- docs/.vuepress/theme/components/Home.vue
+++ docs/.vuepress/theme/components/Home.vue
@@ -1,12 +1,159 @@
 <template>
-  <main class="home" aria-labelledby="main-title">
-    <div class="styled-font">
-      <p>I AM</p>
-      <span id="typed"></span>
-    </div>
+<main class="home" aria-labelledby="main-title">
+  <body class="is-preload">
+    <!-- Header -->
+    <header id="header">
+      <div class="inner">
+        <a href="#" class="image avatar">
+          <img src="./assets/images/avatar.jpg" alt="Avatar" />
+        </a>
+        <h1>
+          <strong>I am Zhang Ziliang</strong>
+          <div>
+            I like
+            <span id="typed"></span>
+          </div>
+        </h1>
+      </div>
+    </header>
 
-    <Content class="theme-default-content custom" />
-  </main>
+    <!-- Main -->
+    <div id="main">
+      <!-- One -->
+      <section id="one">
+        <header class="major">
+          <h2>
+            Ipsum lorem dolor aliquam ante commodo
+            <br />magna sed accumsan arcu neque.
+          </h2>
+        </header>
+        <p>Accumsan orci faucibus id eu lorem semper. Eu ac iaculis ac nunc nisi lorem vulputate lorem neque cubilia ac in adipiscing in curae lobortis tortor primis integer massa adipiscing id nisi accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque cubilia.</p>
+        <ul class="actions">
+          <li>
+            <a href="#" class="button">Learn More</a>
+          </li>
+        </ul>
+      </section>
+
+      <!-- Two -->
+      <!-- <section id="two">
+        <h2>Recent Work</h2>
+        <div class="row">
+          <article class="col-6 col-12-xsmall work-item">
+            <a href="images/fulls/01.jpg" class="image fit thumb">
+              <img src="images/thumbs/01.jpg" alt />
+            </a>
+            <h3>Magna sed consequat tempus</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+          <article class="col-6 col-12-xsmall work-item">
+            <a href="images/fulls/02.jpg" class="image fit thumb">
+              <img src="images/thumbs/02.jpg" alt />
+            </a>
+            <h3>Ultricies lacinia interdum</h3>
+            <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
+          </article>
+        </div>
+        <ul class="actions">
+          <li>
+            <a href="#" class="button">Full Portfolio</a>
+          </li>
+        </ul>
+      </section>-->
+
+      <!-- Three -->
+      <!-- <section id="three">
+        <h2>Get In Touch</h2>
+        <p>Accumsan pellentesque commodo blandit enim arcu non at amet id arcu magna. Accumsan orci faucibus id eu lorem semper nunc nisi lorem vulputate lorem neque lorem ipsum dolor.</p>
+        <div class="row">
+          <div class="col-8 col-12-small">
+            <form method="post" action="#">
+              <div class="row gtr-uniform gtr-50">
+                <div class="col-6 col-12-xsmall">
+                  <input type="text" name="name" id="name" placeholder="Name" />
+                </div>
+                <div class="col-6 col-12-xsmall">
+                  <input type="email" name="email" id="email" placeholder="Email" />
+                </div>
+                <div class="col-12">
+                  <textarea name="message" id="message" placeholder="Message" rows="4"></textarea>
+                </div>
+              </div>
+            </form>
+            <ul class="actions">
+              <li>
+                <input type="submit" value="Send Message" />
+              </li>
+            </ul>
+          </div>
+          <div class="col-4 col-12-small">
+            <ul class="labeled-icons">
+              <li>
+                <h3 class="icon solid fa-home">
+                  <span class="label">Address</span>
+                </h3>1234 Somewhere Rd.
+                <br />Nashville, TN 00000
+                <br />United States
+              </li>
+              <li>
+                <h3 class="icon solid fa-mobile-alt">
+                  <span class="label">Phone</span>
+                </h3>000-000-0000
+              </li>
+              <li>
+                <h3 class="icon solid fa-envelope">
+                  <span class="label">Email</span>
+                </h3>
+                <a href="#">hello@untitled.tld</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </section>-->
+    </div>
+    <footer id="footer">
+      <div class="inner">
+        <ul class="icons">
+          <li>
+            <a href="#" class="icon brands fa-twitter">
+              <span class="label">Twitter</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="icon brands fa-github">
+              <span class="label">Github</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="icon brands fa-dribbble">
+              <span class="label">Dribbble</span>
+            </a>
+          </li>
+          <li>
+            <a href="#" class="icon solid fa-envelope">
+              <span class="label">Email</span>
+            </a>
+          </li>
+        </ul>
+        <ul class="copyright">
+          <li>&copy; Untitled</li>
+          <li>
+            Design:
+            <a href="http://html5up.net">HTML5 UP</a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+
+    <!-- Scripts -->
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.poptrox.min.js"></script>
+    <script src="assets/js/browser.min.js"></script>
+    <script src="assets/js/breakpoints.min.js"></script>
+    <script src="assets/js/util.js"></script>
+    <script src="assets/js/main.js"></script>
+  </body>
+</main>
 </template>
 
 <script>
@@ -21,13 +168,18 @@ export default {
   },
   mounted() {
     new Typed(".home #typed", {
-      strings: ["A Coder", "A cooker", "A leaner"],
+      strings: ["coding", "cooking", "hiking"],
       typeSpeed: 40,
+      shuffle: true,
       loop: true
     });
   }
 };
 </script>
+
+<style scoped>
+@import "./assets/css/home.css";
+</style>
 
 <style lang="stylus">
 .home {
@@ -35,21 +187,5 @@ export default {
   max-width: $homePageWidth;
   margin: 0px auto;
   display: block;
-
-  .styled-font {
-    font-size: 65px;
-    // color: #FFFFFF;
-    // font-family : lulo-clean-w01-one-bold, sans-serif;
-  }
-}
-
-@media (max-width: $MQMobile) {
-  .home {
-  }
-}
-
-@media (max-width: $MQMobileNarrow) {
-  .home {
-  }
 }
 </style>
